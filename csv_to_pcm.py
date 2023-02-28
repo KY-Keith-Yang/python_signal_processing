89850dbbf244b5ff810b9cb428b5ca1dfcb51258
Keith Yang <keith.yang@OSM.group>
2023-02-28 10:05:19 +0800

    add some comment to cs_to_pcm function

--- csv_to_pcm.py
+++ csv_to_pcm.py
@@ -1,3 +1,7 @@
+# 20230228 Keith Yang
+# v1 can easily transform iis csv file(from logic analyzer) to bin file
+# note: bin file is little-endian
+
 import csv
 import struct
 file_name = 'D:\\440hz_0db_2ch_iis.csv'
